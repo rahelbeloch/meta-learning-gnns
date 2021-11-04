9334deaf76e4fbae904b62d0ad07d7f11c7fe53a
Rahel Habacker <rahel@habacker.de>
2021-11-05 00:23:49 +0100

    Addiing nltk, pytorch lightning etc.

--- install-gnn-env.sh
+++ install-gnn-env.sh
@@ -9,17 +9,19 @@ conda env remove -y -n gnn-env
 # maybe try to install the other env and then install these things in that env
 #conda env create -f env.yaml
 
-conda create -y -n gnn-env python=3.7.5 pytorch==1.7.1 pytorch-lightning==1.5.0 dgl~=0.6.1 nltk==3.6.5
+conda create -y -n gnn-env python=3.8.8 pytorch-lightning==1.5.0 dgl~=0.6.1 nltk==3.6.5
 
 #conda activate gnn-env
 
 source activate gnn-env
 
-pip3 install torch==1.7.1+cu101 -f https://download.pytorch.org/whl/torch_stable.html
-pip3 install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.7.0+cu101.html
-pip3 install torch-sparse -f https://pytorch-geometric.com/whl/torch-1.7.0+cu101.html
-pip3 install torch-cluster -f https://pytorch-geometric.com/whl/torch-1.7.0+cu101.html
-pip3 install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-1.7.0+cu101.html
-pip3 install torch-geometric
+conda install pytorch cudatoolkit=10.2 -c pytorch
+
+pip3 install --no-cache-dir torch==1.8.1+cu102 -f https://download.pytorch.org/whl/torch_stable.html
+pip3 install --no-cache-dir torch-scatter -f https://pytorch-geometric.com/whl/torch-1.7.0+cu102.html
+pip3 install --no-cache-dir torch-sparse -f https://pytorch-geometric.com/whl/torch-1.7.0+cu102.html
+pip3 install --no-cache-dir torch-cluster -f https://pytorch-geometric.com/whl/torch-1.7.0+cu102.html
+pip3 install --no-cache-dir torch-spline-conv -f https://pytorch-geometric.com/whl/torch-1.7.0+cu102.html
+pip3 install --no-cache-dir torch-geometric
 
 #pip3 install -r requirements.txt
