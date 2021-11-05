2f4b228a948042c251051667dff966b46bcb9ade
Rahel Habacker <rahel@habacker.de>
2021-11-05 09:57:16 +0100

    Fixing install-gnn-env for cuda 10.2 and pytorch 1.8.1.

--- install-gnn-env.sh
+++ install-gnn-env.sh
@@ -9,22 +9,22 @@ conda env remove -y -n gnn-env
 # maybe try to install the other env and then install these things in that env
 #conda env create -f env.yaml
 
-conda create -y -n gnn-env python=3.7.5 pytorch-lightning==1.5.0 nltk==3.6.5
-
-#conda activate gnn-env
+conda create -y -n gnn-env python=3.7.5 pytorch-lightning==1.5.0 nltk==3.6.5 pytorch==1.8.1
 
 source activate gnn-env
 
+# TODO: also install GPU version of DGL?
+conda install -c dglteam dgl-cuda10.2
+conda install pandas
+
+# install the correct pytorch version (for cuda and not for cpu!); Pytorch Cuda 10.2 and torch 1.8.1 work well together
 conda install pytorch cudatoolkit=10.2 -c pytorch
 
 pip3 install --no-cache-dir torch==1.8.1+cu102 -f https://download.pytorch.org/whl/torch_stable.html
-#pip3 install --no-cache-dir torch-scatter -f https://pytorch-geometric.com/whl/torch-1.7.0+cu102.html
-pip3 install --no-cache-dir torch-sparse -f https://pytorch-geometric.com/whl/torch-1.7.0+cu102.html
-#pip3 install --no-cache-dir torch-cluster -f https://pytorch-geometric.com/whl/torch-1.7.0+cu102.html
-#pip3 install --no-cache-dir torch-spline-conv -f https://pytorch-geometric.com/whl/torch-1.7.0+cu102.html
+pip3 install --no-cache-dir torch-scatter -f https://pytorch-geometric.com/whl/torch-1.8.1+cu102.html
+pip3 install --no-cache-dir torch-sparse -f https://pytorch-geometric.com/whl/torch-1.8.1+cu102.html
+#pip3 install --no-cache-dir torch-cluster -f https://pytorch-geometric.com/whl/torch-1.8.1+cu102.html
+#pip3 install --no-cache-dir torch-spline-conv -f https://pytorch-geometric.com/whl/torch-1.8.1+cu102.html
 pip3 install --no-cache-dir torch-geometric
-
-pip3 install --no-cache-dir dgl
-pip3 install --no-cache-dir pandas
 
 #pip3 install -r requirements.txt
