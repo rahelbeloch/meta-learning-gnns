9d4c093af916edb56efc3a1779cc8fffdf7346a8
Rahel Habacker <rahel@habacker.de>
2021-11-22 18:46:18 +0100

    Fixing version issues in env install process.

--- install-gnn-env-foo.sh
+++ install-gnn-env-foo.sh
@@ -10,11 +10,10 @@ conda env remove -y -n gnn-env-foo
 # maybe try to install the other env and then install these things in that env
 #conda env create -f env.yaml
 
-conda create -y -n gnn-env-foo python==3.7.5 pytorch-lightning==1.5.0 nltk==3.6.5
+conda create -y -n gnn-env-foo python==3.7.5 pytorch-lightning==1.5.0 nltk==3.6.5 pytorch==1.7.1
 
 source activate gnn-env-foo
 
-# TODO: also install GPU version of DGL?
 conda install -c dglteam dgl-cuda10.1
 conda install pandas
 
@@ -24,9 +23,9 @@ pip3 install torchtext==0.8.0
 conda install -c anaconda cudatoolkit=10.1
 
 pip3 install torch==1.7.1+cu101 -f https://download.pytorch.org/whl/torch_stable.html
-pip3 install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.7.0+cu101.html
+#pip3 install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.7.0+cu101.html
 pip3 install torch-sparse -f https://pytorch-geometric.com/whl/torch-1.7.0+cu101.html
-pip3 install --no-cache-dir torch-geometric
+pip3 install torch-geometric
 
 #pip3 install --no-cache-dir torch==1.8.1+cu101 -f https://download.pytorch.org/whl/torch_stable.html
 #pip3 install --no-cache-dir torch-scatter -f https://pytorch-geometric.com/whl/torch-1.8.1+cu101.html
