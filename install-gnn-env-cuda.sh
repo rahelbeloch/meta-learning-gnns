1a11ca0d9422f96ce0f0ab4d14e9607d3d4c86ff
Rahel Habacker <rahel@habacker.de>
2021-11-26 13:00:57 +0100

    Renaming meta env in installation file.

--- install-gnn-env-cuda.sh
+++ install-gnn-env-cuda.sh
@@ -5,14 +5,14 @@ module load 2019
 module load CUDA/10.1.243
 module load Python/3.7.5-foss-2019b
 
-conda env remove -y -n gnn-env-foo
+conda env remove -y -n meta-gnn
 
 # maybe try to install the other env and then install these things in that env
 #conda env create -f env.yaml
 
-conda create -y -n gnn-env-foo python==3.7.5 pytorch-lightning==1.5.3 nltk==3.6.5 pytorch==1.7.1
+conda create -y -n meta-gnn python==3.7.5 pytorch-lightning==1.5.3 nltk==3.6.5 pytorch==1.7.1
 
-source activate gnn-env-foo
+source activate meta-gnn
 
 conda install -c dglteam dgl-cuda10.1
 conda install pandas
