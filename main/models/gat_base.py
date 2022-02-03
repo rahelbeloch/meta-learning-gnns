4c42f4f82e3ebae9d000ce97b00954bdf84d058d
Rahel Habacker <rahel@habacker.de>
2022-02-03 10:59:50 +0100

    Running some experiments.

--- main/models/gat_base.py
+++ main/models/gat_base.py
@@ -89,18 +89,20 @@ class GatBase(pl.LightningModule):
 
     def validation_epoch_end(self, outputs) -> None:
         super().validation_epoch_end(outputs)
-        self.log_f1(outputs)
-
-    def test_epoch_end(self, outputs) -> None:
-        super().test_epoch_end(outputs)
-        print(outputs)
-        self.log_f1(outputs)
+        self.log_f1(outputs, 'val')
 
     def training_epoch_end(self, outputs) -> None:
-        super().training_epoch_end(outputs)
-        self.log_f1(outputs)
+        super().test_epoch_end(outputs)
+        self.log_f1(outputs, 'train')
 
-    def log_f1(self, outputs):
+    def test_epoch_end(self, outputs) -> None:
+        super().training_epoch_end(outputs)
+        test_metrics = outputs[0]
+        self.log_f1(test_metrics, 'test')
+        val_test_metrics = outputs[1]
+        self.log_f1(val_test_metrics, 'test_val')
+
+    def log_f1(self, outputs, mode):
         """
         Outputs is a list of dicts: {'loss': tensor(0.3119, device='cuda:0'), 'f1': 0.04878048780487806}
         """
@@ -108,7 +110,7 @@ class GatBase(pl.LightningModule):
         f1_scores = torch.FloatTensor([d['f1'] for d in outputs if d['f1'] is not None])
         # print(f"\nF1 scores: {str(f1_scores)}")
         f1_mean = f1_scores.mean()
-        self.log('f1', f1_mean, on_step=False, on_epoch=True)
+        self.log(f'{mode}_f1', f1_mean, on_step=False, on_epoch=True)
         # print(f"Averaged F1 score for {len(f1_scores)} batches: {f1_mean}\n")
 
     def forward(self, sub_graphs, mode=None):
