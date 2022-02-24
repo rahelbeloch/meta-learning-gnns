7309b420f78bb314451d5abd872e4ef3282fde22
Rahel Habacker <rahel@habacker.de>
2022-02-24 12:24:00 +0100

    Training baseline only on query set.

--- main/models/gat_base.py
+++ main/models/gat_base.py
@@ -89,7 +89,7 @@ class GatBase(GraphTrainer):
             sub_graphs = support_graphs + query_graphs
             targets = torch.cat([support_targets, query_targets])
         else:
-            print(f"\nEvaluating {type(self.model).__name__.split('.')[-1]} on {len(sub_graphs)}"
+            print(f"\n{mode} {type(self.model).__name__.split('.')[-1]} on {len(sub_graphs)}"
                   f"samples (only query set).")
 
         # make a batch out of all sub graphs and push the batch through the model
