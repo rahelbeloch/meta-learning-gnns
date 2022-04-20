9eebd908bf8328e7f3653b260e93003a6b85c9c5
Rahel Habacker <rahel@habacker.de>
2022-04-20 10:44:06 +0200

    Gat finetune on validation support set.

--- main/train_gnn.py
+++ main/train_gnn.py
@@ -436,6 +436,8 @@ if __name__ == "__main__":
 
     os.environ["WANDB_MODE"] = params['wb_mode']
 
+    wandb.init(settings=wandb.Settings(start_method="fork"))
+
     train(
         balance_data=not params['no_balance_data'],
         progress_bar=params['progress_bar'],
