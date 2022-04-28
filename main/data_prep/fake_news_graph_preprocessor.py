e20434fa2d9648c1008d45656e8474bf2a5ddde5
Rahel Habacker <rahel@habacker.de>
2022-04-28 13:01:40 +0200

    Balancing data

--- main/data_prep/fake_news_graph_preprocessor.py
+++ main/data_prep/fake_news_graph_preprocessor.py
@@ -158,9 +158,6 @@ if __name__ == '__main__':
 
     parser.add_argument('--test-size', dest='test_size', type=float, default=0.2, help='Size of train split.')
 
-    parser.add_argument('--balance-data', dest='balance_data', type=bool, default=False,
-                        help='If dataset imbalance should be balanced out or not.')
-
     args, unparsed = parser.parse_known_args()
 
     preprocessor = FakeNewsGraphPreprocessor(args.__dict__)
--- main/data_prep/fake_news_tsv_processor.py
+++ main/data_prep/fake_news_tsv_processor.py
@@ -199,7 +199,7 @@ if __name__ == '__main__':
 
     parser.add_argument('--test-size', dest='test_size', type=float, default=0.2, help='Size of train split.')
 
-    parser.add_argument('--balance-data', dest='balance_data', type=bool, default=False,
+    parser.add_argument('--balance-data', dest='balance_data', type=bool, default=True,
                         help='If dataset imbalance should balanced out or not.')
 
     parser.add_argument('--balance-val', dest='balance_val', type=bool, default=True,
