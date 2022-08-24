a2d30ed67e6a23ce5f0ca55cfb8e41d7cd2b16b8
Rahel Habacker <rahel@habacker.de>
2022-08-25 01:47:50 +0200

    Working on same query sets.

--- main/data_prep/validation.py
+++ main/data_prep/validation.py
@@ -12,7 +12,7 @@ from train_config import SHOTS
 
 data_train = 'gossipcop'
 data_eval = 'gossipcop'
-model_name = 'gat'
+model_name = 'maml'
 h_size = 2
 top_users, top_users_excluded = 30, 1
 k_shot = 5
@@ -38,12 +38,7 @@ def sub_graphs_loader_validation():
                     'Center idx stored in wrong index per class!'
 
 
-shot_batch_size_map = {
-    5: 8112,
-    10: 8349,
-    20: 4221,
-    40: 8442
-}
+gat_train_batches = 1
 
 
 def validate_query_set_equal():
@@ -51,30 +46,34 @@ def validate_query_set_equal():
     for k in SHOTS:
         loaders, train_graph, eval_graph = get_data(data_train, data_eval, model_name, h_size, top_users,
                                                     top_users_excluded, k, train_split_size, eval_split_size,
-                                                    feature_type, vocab_size, dirs, shot_batch_size_map[k], num_workers)
+                                                    feature_type, vocab_size, dirs, gat_train_batches, num_workers)
 
         train_loader, train_val_loader, test_loader, test_val_loader = loaders
 
         query_nodes, support_nodes = [], []
-        for episode in iter(train_loader):
-            if model_name == 'gat':
-                sub_graphs, _ = episode
-                half_len = int(len(sub_graphs) / 2)
-                query_sub_graphs = sub_graphs[half_len:]
-            else:
-                support_sub_graphs, query_sub_graphs, _, _ = episode
-            query_nodes += [(graph.orig_center_idx, graph.target, graph.set_type) for graph in query_sub_graphs]
+        for episode in iter(test_loader):
+            # if model_name == 'gat' and mode != 'text':
+            #     # sub graphs are organizes as: [s,s,s,s,s... q,q,q,q, ..., s,s,s,s, ...q,q,q,q ...]
+            #     sub_graphs, targets = episode
+            #     for i, g in enumerate(sub_graphs):
+            #         if g.set_type == 'query':
+            #             query_nodes.append((g.orig_center_idx, targets[i].item(), g.set_type))
+            # else:
+            support_sub_graphs, query_sub_graphs, support_targets, query_targets = episode
+            for i, g in enumerate(query_sub_graphs):
+                query_nodes.append((g.orig_center_idx, query_targets[i].item(), g.set_type))
+
         query_shot_nodes[k] = query_nodes
         print(f"\nCollected {len(query_nodes)} query nodes for shot '{k}'")
 
-    difference_5_10 = set(query_shot_nodes[5]).symmetric_difference(set(query_shot_nodes[10]))
-    difference_5_20 = set(query_shot_nodes[5]).symmetric_difference(set(query_shot_nodes[20]))
-    difference_5_40 = set(query_shot_nodes[5]).symmetric_difference(set(query_shot_nodes[40]))
+    difference_5_10 = set(query_shot_nodes[4]).symmetric_difference(set(query_shot_nodes[8]))
+    difference_5_20 = set(query_shot_nodes[4]).symmetric_difference(set(query_shot_nodes[12]))
+    difference_5_40 = set(query_shot_nodes[4]).symmetric_difference(set(query_shot_nodes[16]))
 
-    difference_10_20 = set(query_shot_nodes[10]).symmetric_difference(set(query_shot_nodes[20]))
-    difference_10_40 = set(query_shot_nodes[10]).symmetric_difference(set(query_shot_nodes[40]))
+    difference_10_20 = set(query_shot_nodes[8]).symmetric_difference(set(query_shot_nodes[12]))
+    difference_10_40 = set(query_shot_nodes[8]).symmetric_difference(set(query_shot_nodes[16]))
 
-    difference_20_40 = set(query_shot_nodes[20]).symmetric_difference(set(query_shot_nodes[40]))
+    difference_20_40 = set(query_shot_nodes[12]).symmetric_difference(set(query_shot_nodes[16]))
 
     assert len(difference_5_10) == 0
     assert len(difference_5_20) == 0
@@ -82,6 +81,13 @@ def validate_query_set_equal():
     assert len(difference_10_20) == 0
     assert len(difference_10_40) == 0
     assert len(difference_20_40) == 0
+
+    print(f"Difference 4 and 8: {difference_5_10}")
+    print(f"Difference 4 and 12: {difference_5_20}")
+    print(f"Difference 4 and 16: {difference_5_40}")
+    print(f"Difference 8 and 12: {difference_10_20}")
+    print(f"Difference 8 and 16: {difference_10_40}")
+    print(f"Difference 12 and 16: {difference_20_40}")
 
 
 def check_train_loader_query_samples():
@@ -201,7 +207,7 @@ def node_indices_belong_to_split():
 if __name__ == '__main__':
     # check_train_loader_query_samples()
 
-    # validate_query_set_equal()
+    validate_query_set_equal()
 
     # data_config = {'top_users': top_users, 'top_users_excluded': top_users_excluded, 'feature_type': feature_type,
     #                'vocab_size': vocab_size}
@@ -217,4 +223,4 @@ if __name__ == '__main__':
 
     # visualize_subgraphs()
 
-    node_indices_belong_to_split()
+    # node_indices_belong_to_split()
--- main/train_config.py
+++ main/train_config.py
@@ -1,7 +1,14 @@
 META_MODELS = ['gmeta', 'proto-maml', 'maml']
 SUPPORTED_MODELS = ['gat', 'prototypical'] + META_MODELS
-# SHOTS = [5, 8, 10, 16, 20, 40]
+
 # SHOTS = [5, 10, 20, 40]
 SHOTS = [4, 8, 12, 16]
+
+GOSSIPCOP_QUERY_SAMPLES = {'train': {0: 1640, 1: 820}, 'val': {0: 200, 1: 50}, 'test': {0: 480, 1: 120}}
+GOSSIPCOP_EPISODES = {
+    'train': {4: 820, 8: 410, 12: 205, 16: 205},
+    'val': {4: 50, 8: 25, 12: 25, 16: 25},
+    'test': {4: 120, 8: 60, 12: 40, 16: 20}
+}
 
 LOG_PATH = "../logs/"
